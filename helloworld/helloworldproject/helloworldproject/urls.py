aee43deb906b6252fb530a2d319cc25b34ec2219
s.konishi <kakikukekonisi2599@gmail.com>
2020-02-12 09:46:06 +0900

    remove hello

--- helloworld/helloworldproject/helloworldproject/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from .views import helloworldfunction, HelloWorldView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/', include('helloworldapp.urls'))
-]
--- helloworld/helloworldproject/helloworldproject/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.views.generic import TemplateView
-
-def helloworldfunction(request):
-    returnobject = HttpResponse('<h1>hello,world!ですよ</h1>')
-    return returnobject
-
-class HelloWorldView(TemplateView):
-    template_name = 'hello.html'
