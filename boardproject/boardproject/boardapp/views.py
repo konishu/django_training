c586d4746eec0b1968d0400ea2995a4200bef811
s.konishi <kakikukekonisi2599@gmail.com>
2020-02-26 15:24:41 +0900

    finish chap5git add -A

--- boardproject/boardproject/boardapp/views.py
+++ boardproject/boardproject/boardapp/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth import authenticate, login,logout
 from django.shortcuts import redirect
 from .models import BoardModel
 from django.contrib.auth.decorators import login_required
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
 def signupfunc(request):
     if request.method == 'POST':
@@ -38,3 +40,30 @@ def logoutfunc(request):
     logout(request)
     return redirect('login')
     # Redirect to a success page.
+
+def detailfunc(request, pk):
+    object = BoardModel.objects.get(pk=pk)
+    return render(request, 'detail.html', {'object':object})
+
+def goodfunc(request, pk):
+    post = BoardModel.objects.get(pk=pk)
+    post.good = post.good + 1
+    post.save()
+    return redirect('list')
+
+def readfunc(request, pk):
+    post = BoardModel.objects.get(pk=pk)
+    post2 = request.user.get_username()
+    if post2 in post.readtext:
+        return redirect('list')
+    else:
+        post.read += 1
+        post.readtext = post.readtext + '' + post2
+        post.save
+        return redirect('list')
+
+class BoardCreate(CreateView):
+    template_name = 'create.html'
+    model = BoardModel
+    fields = ('title','content','author','images')
+    success_url = reverse_lazy('list')
